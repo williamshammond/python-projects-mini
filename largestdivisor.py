78e2da2f648b219aeec6003b542ee20a00c9b747
williamshammond <williamhammond44@gmail.com>
2020-10-26 21:50:54 -0700

    Update largestdivisor.py

--- largestdivisor.py
+++ largestdivisor.py
@@ -1,9 +1,13 @@
 def find_largest_divisor(n):
     n = n
-    print(n)
+    largest_divisor = 1
+    for x in range(int(n/2)+1,0,-1):
+        if(n%x==0):
+            largest_divisor = x
+            return largest_divisor
 
 def main():
-    find_largest_divisor(10)
+    print(find_largest_divisor(10))
 
 if __name__ == '__main__':
     main()
